4d7e1ca1b69a81e4e5c0f0e6e331b708046d4c78
hnp36 <hnp36@njit.edu>
2025-02-04 21:10:16 -0500

    update

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -6,9 +6,11 @@ def add(a,b):
 def substract(a,b):
     """this is substaction """
     return a - b
+
 def division(a,b):
     """this is division"""
     return a / b
+
 def multiplication(a,b):
     """this is multiplication"""
     return a * b
